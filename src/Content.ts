a1e419d73821b45d842cb1356a9ea53f2d807b19
dezbence <dezbence@gmail.com>
2020-03-27 12:18:34 +0100

    up

--- src/Content.ts
+++ src/Content.ts
@@ -26,8 +26,9 @@ export default class Content {
         // Kezd a kódolást innen -->
 
         res.write("Hello Jedlik, hello 9.A");
-        res.write("<h2 style='color:red;'>TipeScript</h2>");
-
+        res.write("<h2 style='color:blue;'>TipeScript</h2>");
+        res.write("próba\n")
+        res.write("alma\n")
         // <---- Fejezd be a kódolást
 
         res.write("</pre></form></body></html>");
